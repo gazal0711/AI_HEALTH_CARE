56147b961375a6c602efab9d8cfc700ff29729b0
gazal0711 <gazalkmard@gmail.com>
2025-02-15 01:19:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from transformers import pipeline
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+chatbot = pipeline("text-generation", model="distilgpt2")
+
+
+
+def healthcare_chatbot(user_input):
+    
+    if "symptom" in user_input:
+        return "It seems like you're experiencing symptoms. Please consult a doctor for accurate advice."
+    elif "appointment" in user_input:
+        return "Would you like me to schedule an appointment with a doctor?"
+    elif "medication" in user_input:
+        return "It's important to take your prescribed medications regularly. If you have concerns, consult your doctor."
+    else:
+        # For other inputs, use the Hugging Face model to generate a response
+        response = chatbot(user_input, max_length=500, num_return_sequences=1)
+        # Specifies the maximum length of the generated text response, including the input and the generated tokens.
+        # If set to 3, the model generates three different possible responses based on the input.
+        return response[0]['generated_text']
+
+
+def main():
+    
+    st.title("Healthcare Assistant Chatbot")
+    
+    
+    user_input = st.text_input("How can I assist you today?", "")
+    
+    
+    if st.button("Submit"):
+        if user_input:
+            st.write("User: ", user_input)
+            # response = healthcare_chatbot(user_input)
+            with st.spinner("Processing your query, please wait..."):
+                response = healthcare_chatbot(user_input)
+            print("response ",response)
+            print("=======================================================")
+            st.write("Healthcare Assistant: ", response)
+        else:
+            st.write("Please enter a query.")
+if __name__ == "__main__":
+    main()
